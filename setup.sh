be194d76537edbeb13eeae0f50417f598b27f0be
davismatrix <davismatrix@gmail.com>
2025-04-05 19:40:09 -0400

    add jenkins file for cd

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ fi
 
 # Install Java 8, Java 11 & Docker
 apt update
-apt install -y openjdk-8-jdk openjdk-11-jdk docker.io maven
+apt install -y openjdk-8-jdk openjdk-11-jdk openjdk-17-jdk docker.io maven gnupg lsb-release curl
 usermod -a -G docker ubuntu
 
 # Install Trivy
