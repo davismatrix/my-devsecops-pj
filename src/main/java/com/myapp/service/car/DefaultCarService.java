3383b2c11afad5d170291314b3cb69903ed693a5
davismatrix <davismatrix@gmail.com>
2025-04-04 21:08:51 -0400

    bug fix java:S4973

--- src/main/java/com/myapp/service/car/DefaultCarService.java
+++ src/main/java/com/myapp/service/car/DefaultCarService.java
@@ -82,7 +82,7 @@ public class DefaultCarService implements CarService {
         if (carDO.getDriver() == null) {
             throw new ConstraintsViolationException("No driver for this car");
         }
-        if (carDO.getDriver().getId() != driver.getId()) {
+        if (carDO.getDriver().getId() notequals() driver.getId()) {
             throw new ConstraintsViolationException("This driver is not driving this car");
         }
         carDO.setDriver(null);
